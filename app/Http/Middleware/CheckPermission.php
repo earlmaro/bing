bbe0ad926daddf9a220ca6dfe74f25ba62e49be7
earl_maro <tobiprecious13@gmail.com>
2022-05-24 22:48:15 +0100

    initial commit

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Helpers\Traits\ApiResponseTrait;
+use Closure;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+
+class CheckPermission
+{
+    use ApiResponseTrait;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $userType)
+    {
+        if (strtolower($request->method()) === 'options') {
+            return $next($request);
+        }
+
+        if ($this->check_access($request, $userType) !== true) {
+            return $this->abort();   
+        }
+
+        return $next($request);
+    }
+
+    private function check_access($request, $userType) 
+    {
+        if (! ($user = $request->user()) || strtolower($user->type) !== $userType) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function abort($message = 'Access Forbidden')
+    {
+        return $this->respondForbidden($message);
+    }
+}
